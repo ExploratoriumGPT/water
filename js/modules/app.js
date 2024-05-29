a20b639644ca17bb21017cdbc1e55d54d54481b2
Gregory Cowley <gregory@cowleystudios.com>
2024-05-29 14:35:33 -0700

    Debugging and refectoring

--- js/modules/app.js
+++ js/modules/app.js
@@ -28,239 +28,208 @@
 
 import { connectSerial, serialSend } from './serial.js';
 
-
 let pages;
 let currentStep = 0;
-let currentPage;
 let timeoutId;
-let port;
+let serialPort;
+let largeTankState = '';
 
-
-// const button = document.getElementById('button');
-
-// button.addEventListener('click', async () => {
-//   await incrementPage();
-// });
-
-// document.getElementById('resetButton').addEventListener('click', function () {
-//   reset();
-// });
-
+// Dynamic DOM Elements
+const imageElement = document.getElementById('image');
+const resetButton = document.getElementById('resetButton');
 const actionButton = document.getElementById('button');
+
 actionButton.addEventListener('click', handleActionButtonClick);
 
 function loadPages() {
     fetch('../data/pages.json')
-        .then(response => response.json())
-        .then(obj => begin(obj))
-        .catch(error => {
-            // Handle any errors that occurred during the fetch
-            console.error('Fetch error:', error);
-        });
+    .then(response => response.json())
+    .then(obj => begin(obj))
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('Fetch error:', error);
+    });
 }
 
 function begin(obj) {
     pages = obj;
-    const page = pages[currentStep];
-    console.log('---- Begin :: ', page);
-   
-    updatePage( page );
+    updatePage( currentStep );
+    // resetTimeout();
 }
+
+function incrementPage() {
+    currentStep++;
+    logInfo(`---- Increment page #${currentStep} ---- `);
+    updatePage( currentStep );
+}
+
+function updatePage( pageIndex ) {
+    const nextPage = pages[pageIndex];
+    logStatus(nextPage.stage);
+    
+    logInfo("Updating page: ", nextPage);
+    logInfo(`This page has an increment command of: ${nextPage.pageIncrementCommand}`);
+    
+    // Image
+    imageElement.src = nextPage.graphicsPath;
+    
+    if ( nextPage.pageLoadedCommand !== undefined && nextPage.pageLoadedCommand !== "") {
+        serialSend( serialPort, nextPage.pageLoadedCommand );
+    }
+    
+    // Button
+    actionButton.textContent = nextPage.actionText;
+    actionButton.disabled = nextPage.lockout;
+    if (nextPage.resetVisible){
+        resetButton.style.visibility = 'visible';
+        resetButton.disabled = nextPage.lockout;
+    } else {
+        resetButton.style.visibility = 'hidden';
+        resetButton.disabled = nextPage.lockout;
+    }
+}
+
+/*
+
+Events:
+
+Actions that are triggered at unpredictable times.
+
+- User interactions events, like button clicks.
+- Serial data events.
+
+*/
 
 function handleActionButtonClick() {
     resetTimeout();
     incrementPage();
 }
 
-function incrementPage() {
-    currentStep++;
-    const nextPage = pages[currentStep];
-    console.log('---- Increment page :: ', nextPage);
-    updatePage( nextPage );
+document.addEventListener('serialready', handleSerialReady);
+function handleSerialReady(event) {
+    logInfo('Serial Ready Event: ', event);
+    serialPort = event.detail.port;
 }
 
-function updatePage( page ) {
-    updateStatus(page.stage);
-    console.log("Updating page: ", page);
-    updateFeedback(`This page has a next command of: ${page.nextCommand}`);
-  
-    const actionButton = document.getElementById('button');
-    const image = document.getElementById('image');
-    const resetButton = document.getElementById('resetButton');
-    
-    // Image
-    image.src = page.graphicsPath;
-
-    if ( page.commandToSend !== undefined && page.commandToSend !== "") {
-        serialSend( port, page.commandToSend );
-    }
-  
-    // Button
-    actionButton.textContent = page.actionText;
-    actionButton.disabled = page.lockout;
-    if (page.resetVisible){
-      resetButton.style.visibility = 'visible';
-      resetButton.disabled = page.lockout;
-    } else {
-      resetButton.style.visibility = 'hidden';
-      resetButton.disabled = page.lockout;
-    }
-  }
-  
-  function updateTitle(text) {
-      console.log('Updating title to: ', text);
-      // const title = document.getElementById('title');
-      // title.textContent = text;
-  }
-  
-  function updateStatus(text) {
-      console.log('Updating status to: ', text);
-      // const status = document.getElementById('status');
-      // status.textContent = text;
-  }
-  
-  export { 
-    updatePage
-  }
-  
-
-
-
-// Serial Loop
-
 function handleSerialData(data) {
-    
+    logInfo(`Raw data:: ${data}` );
     const oneLetterData = `${/[A-Z]/.exec(data)}`;
     if (!oneLetterData || oneLetterData == 'null') {
-        // updateFeedback(`Received:  No useful data ${data}` );
+        // logInfo(`Received:  No useful data ${data}` );
         return;
     }   
-    updateFeedback(`Received: ${oneLetterData}`);
-
-    updateFeedback(`The current page is expecting command: ${pages[currentStep].nextCommand}`);
-
+    logInfo(`Received ${oneLetterData}, ${pages[currentStep].pageIncrementCommand} will trigger page increment.`);
     // Assume data is formatted as a single character command:
     switch ( oneLetterData ){
         case "N":
-            updateFeedback(`Assuming earth tank is filling... waiting`);
-            // Do nothing here
-            break;
-
+            largeTankState = 'N';
+        logInfo(`Assuming earth tank is filling... Do nothing here`);
+        break;
+        
         case "F":
-            updateFeedback(`Assuming earth tank is full... incementing page`);  
-            // Earth Tank is Full, get ready to drain the earth
-            incrementPage();
-            break; 
+            largeTankState = 'F';
+        logInfo(`FULL, ready to drain. Incrementing page.`);  
+        incrementPage();
+        break; 
         
         case "V":
-            updateFeedback(`Assuming earth tank is draining`);
-            // Do nothing here
-            break;
+            largeTankState = 'V';
+        logInfo(`Assuming earth tank is draining... Do nothing here`);
+        break;
         
         case "E":
-            updateFeedback(`Assuming earth tank is empty`);
-            if ( pages[currentStep].nextCommand == 'E' ) {
-                incrementPage();
-            }
-            // Do nothing here
-            break;
-            
+            largeTankState = 'E';
+        logInfo(`Assuming earth tank is empty... increment page if needed`);
+        if ( pages[currentStep].pageIncrementCommand == 'E' ) {
+            incrementPage();
+        }
+        break;
+        
         case "X":
-            // Earth Tank is Full
-            updateFeedback(`X received, drop drop`, pages[currentStep].stage);
-            if (pages[currentStep].stage == 'PRE_DRIP') {
-                incrementPage();
-            }
-            break;
-
+            largeTankState = 'X';
+        logInfo(`Actively dripping:: `, pages[currentStep].stage);
+        resetTimeout();
+        if (pages[currentStep].stage == 'PRE_DRIP') {
+            incrementPage();
+        }
+        break;
+        
         case "D":
-            // Sink is Dripping
-            break;
-
+            largeTankState = 'D';
+        logInfo(`Actively dripping [Possibly Unused Stage]:: `, pages[currentStep].stage);
+        break;
+        
         default:
-            // Data is not formatted as expected, ignore.
-            updateFeedback(`Received unexpected data: ${oneLetterData}`);
+        // Data is not formatted as expected, ignore.
+        logInfo(`Received unexpected data: ${oneLetterData}`);
     }
 }
-
-function lockUI() {
-    button.disabled = true;
-    resetButton.disabled = true;
-  }
-  
-  function unlockUI() {
-    button.disabled = false;
-    resetButton.disabled = false;
-  }
-
-  function updateFeedback(message, ...args) {
-    console.log(`App Feedback: ${message}`, args);
-  }
-
-
 
 // Reset and Timeout
 
 function resetTimeout() {
-    // Clear the existing timeout
-    // if (timeoutId) {
-    //     clearTimeout(timeoutId);
-    // }
-    
-    // // Set a new timeout
-    // timeoutId = setTimeout(() => {
-    //     console.log('Timeout computer side');
-    //     reset();
-    // }, 55000); // 55 seconds
+    clearTheTimeout();
+    timeoutId = setTimeout(() => {
+        reset('timeout');
+    }, 55000); // 55 seconds
 }
 
-async function reset() {
-    // pageIndex = 0;
-    console.log('reset function called');
-
-    serialSend(port, 'R\n');
-    
-    // if (tankState.name == 'full' || tankState.name == 'filling' || tankState.name == 'draining') {
-    //     console.log('resetting interactive lockout, tank full');
-    //     pageIndex = pages.findIndex(page => page.name == 'RESET');
-    //     await serialSend('T\n');
-    // }
-    // else {
-    //     console.log('resetting interactive FAST, tank not full');
-    //     pageIndex = pages.findIndex(page => page.name == 'READY');
-    // }
-    
-    currentStep = 0;
-    const page = pages[currentStep];
-    updatePage( page );
+function clearTheTimeout() {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
 }
 
+async function reset(reason) {
+    logInfo(`reset function called: ${reason}, tank state: ${largeTankState}`);
+    if (largeTankState == 'E') {
+        logInfo('Tank already empty, ready for next visitor.');
+        currentStep = 1;
+    } else {
+        logInfo('Tank is full, draining...');
+        serialSend(serialPort, 'R\n');
+        currentStep = 0;
+    
+    }
+    updatePage( currentStep );
+}
+
+/* 
+
+Logging
+
+Why go through the trouble of creating logging functions?
+- Logging functions provide a consistent way to log messages.
+- They can be turned off or modified easily.
+- They can be used to add more information like timestamps 
+and the section of the code that is initiating the logging.
+
+*/
+function logStatus(message, ...args) {
+    console.log(`Status [app]: ${message}`, args);
+}
+
+function logInfo(message, ...args) {
+    console.log(`Info [app]: ${message}`, args);
+}
+
+function logError(message, ...args) {
+    console.error(`Error [app]: ${message}`, args);
+}
 
 // Initialize the app
 
 async function init() {
     try {
         const { openPort, stream } = await connectSerial( handleSerialData );
-        port = openPort;
-        console.log('Port opened, stream ready.', port);
+        serialPort = openPort;
+        logInfo('Port opened, stream ready.', serialPort, stream);
         loadPages();
         
     } catch (err) {
-        console.error('Error Initializing: ', err);
-        // reset();
+        logError('[GC DEBUG] Error Initializing: ', err);
+
     }
 }
-
-
-// async function init() {
-//     // console.log('Page Loaded, looking for ports...');
-//     const { openPort, stream } = await connectSerial( handleSerialData );
-//     port = openPort;
-//     console.log('Page Loaded, setupSerial done');
-//     if (port) {
-//         console.log('setupResult happened!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-//         reset();
-//     }
-// }
 
 window.addEventListener('load', init);
--- js/modules/serial.js
+++ js/modules/serial.js
@@ -16,157 +16,180 @@
 * ------------------------------------ */
 
 async function connectSerial( serialCallback ) {
-  if (!navigator.serial) {
-      updateFeedback('Serial not supported in this browser');
-      return;
-  }
-  let connectedPort = null;
-  let readerStream = null;
-  try {
-      addSerialListeners();
-      updateFeedback("Aquiring serial port...");
-      connectedPort = await openSerialPort();
-      readerStream = await applyReaderToPort(connectedPort, serialCallback);
-      checkSignals(connectedPort);
-      return { openPort: connectedPort, stream: readerStream };
-  } catch (err) {
-      console.error('Could not connect to serial port: ', err);
-      displaySerialFixButton();
-  }
+    if (!navigator.serial) {
+        updateFeedback('Serial not supported in this browser');
+        return;
+    }
+    let connectedPort = null;
+    let readerStream = null;
+    try {
+        addSerialListeners();
+        updateFeedback("Aquiring serial port...");
+        connectedPort = await openSerialPort();
+        readerStream = await applyReaderToPort(connectedPort, serialCallback);
+        checkSignals(connectedPort);
+        return { openPort: connectedPort, stream: readerStream };
+    } catch (err) {
+        //   console.error('[GC DEBUG] Could not connect to serial port: ', err);
+        displaySerialFixButton();
+        throw err;
+    }
 }
 
 async function openSerialPort() {
-  // Wait for existing serial ports to be populated
-  const ports = await navigator.serial.getPorts();
-  let connectedPort = null;
-  if (ports.length <= 0) {
-      throw "No serial ports found";
-
-  } else {
-
-    updateFeedback("Ports: ", ports);
-      updateFeedback("Serial ports are avaiable... choosing the first one to open.");
-      connectedPort = ports[0];
-      await connectedPort.open({ baudRate: 115200 });
-      updateFeedback("Serial port opened successfully.");
-
-  }
-  return connectedPort
+    // Wait for existing serial ports to be populated
+    const ports = await navigator.serial.getPorts();
+    let connectedPort = null;
+    if (ports.length > 0) {
+        updateFeedback("Serial ports are avaiable... choosing the first one to open.", ports);
+        connectedPort = ports[0];
+        await connectedPort.open({ baudRate: 115200 });
+        updateFeedback("Serial port opened successfully.");
+        
+    } else {  
+        throw "No serial ports found";
+        
+    }
+    return connectedPort
 }
 
 async function applyReaderToPort(openPort, callback) {
-  updateFeedback("Applying Serial Reader to Port...");
-  async function setupSerialReader(oPort, callback) {
-      // Set up a reader to read data from the serial port
-      // Adding this function here because it is only used by the reader:
-      async function readLoop(controller, reader) {
-          while (true) {
-              // console.log('reading');
-              const { value, done } = await reader.read();
-              if (value) {
-                  callback(new TextDecoder().decode(value));
-              }
-              if (done) {
-                  reader.releaseLock();
-                  break;
-              }
-          }
-      }
-      const reader = await oPort.readable.getReader();
-      const stream = new ReadableStream(
-          {
-              start(controller) {
-                  // The following function handles each data chunk
-                  readLoop(controller, reader);
-              }
-          }
-      );
-      return stream;
-  }
+    if ( !openPort || !openPort.readable ) {
+        throw "Missing an open port to apply the reader to.";
+    }
+    updateFeedback("Applying Serial Reader to Port...");
+    async function setupSerialReader(oPort, callback) {
+        // Set up a reader to read data from the serial port
+        // Adding this function here because it is only used by the reader:
+        async function readLoop(controller, reader) {
+            while (true) {
+                const { value, done } = await reader.read();
+                if (value) {
+                    callback(new TextDecoder().decode(value));
+                }
+                if (done) {
+                    reader.releaseLock();
+                    break;
+                }
+            }
+        }
+        const reader = await oPort.readable.getReader();
+        const stream = new ReadableStream(
+            {
+                start(controller) {
+                    // The following function handles each data chunk
+                    readLoop(controller, reader);
+                }
+            }
+        );
+        return stream;
+    }
 
-  if (openPort) {
-      const readerStream = await setupSerialReader(openPort, callback)
-      updateFeedback("READY!");
-      return readerStream;
-  } else {
-      throw "Missing an open port.";
-  }
+        const readerStream = await setupSerialReader(openPort, callback)
+        updateFeedback("READY!");
+        return readerStream;
+    
 }
 
 async function checkSignals(oPort) {
-  try {
-      const signals = await oPort.getSignals();
-      console.log('Testing Signals:', signals);
-  } catch (err) {
-        updateFeedback('Could not get signals from Serial Port');
-  }
+    try {
+        const signals = await oPort.getSignals();
+        updateFeedback('Testing Signals:', signals);
+    } catch (err) {
+        throw ('Could not get signals from Serial Port', err);
+    }
 }
 
 function addSerialListeners() {
-  navigator.serial.addEventListener("connect", (e) => {
-    updateFeedback('Serial port connected');
-      // Connect to `e.target` or add it to a list of available ports.
-  });
-  navigator.serial.addEventListener("disconnect", (e) => {
-    updateFeedback('Serial port disconnected');
-      // Remove `e.target` from the list of available ports.
-  });
+    navigator.serial.addEventListener("connect", (e) => {
+        updateFeedback('Serial port connected');
+        // Connect to `e.target` or add it to a list of available ports.
+    });
+    navigator.serial.addEventListener("disconnect", (e) => {
+        updateFeedback('Serial port disconnected');
+        // Remove `e.target` from the list of available ports.
+    });
 }
 
 function serialSend(port, char) {
-    console.info('sending ' + char + ' over serial to arduino');
+    updateFeedback(`Sending ${char} over serial to arduino`);
     if (port) {
         const writer = port.writable.getWriter();
         writer.write(new TextEncoder().encode(char));
         writer.releaseLock();
     }
     else {
-        console.info('FAILED to send  ' + char + ' over serial');
+        throw `FAILED to send ${char} over serial. No serial port available.`
     }
 }
 
 
+/* ------------------------------------------------------------------------
 
+    Display a button to allow the user to connect to a serial port
 
-/* ------------------------------------
-* 
-*  Display a button to allow the user to connect to a serial port
-*  
-* ------------------------------------ */
+    Transient activation is required. The user has to interact with 
+    the page or a UI element in order for this feature to work.
 
+        Note: Transient activation expires after a timeout (if not 
+            renewed by further interaction), and may also be "consumed" 
+            by some APIs. See Sticky activation for a user activation 
+            that doesn't reset after it has been set initially.
+
+    Requires a Secure Context
+    https://developer.mozilla.org/en-US/docs/Web/API/Serial/requestPort
+
+    - must be served over https:// or wss:// URLs
+    - the security properties of the network channel used to deliver the resource must not be considered deprecated
+
+    Locally-delivered resources (http://127.0.0.1,http://localhost and http://*.localhost) 
+    are considered to have been delivered securely.
+
+    ------------------------------------------------------------------------ */
 function displaySerialFixButton() {
-  const serialButton = createSerialButton();
-  async function handleButtonClick() {
-      updateFeedback("Opening serial connection dialog...");
-      const port = await navigator.serial.requestPort();
-      if (port) {
-          console.log('port opened');
-          // Redo the Init function to re-establish the serial connection
-          init();
-          // Clean up the button
-          serialButton.remove();
-      }
-  }
-  serialButton.addEventListener('click', handleButtonClick);
+    if (window.isSecureContext) {
+        updateFeedback('Secure context detected. Displaying serial button...');
+    } else {
+        updateFeedback('Not a secure context. Serial button will not be displayed.');
+    }
+
+    const serialButton = createSerialButton();
+    async function handleButtonClick() {
+        updateFeedback("Opening serial connection dialog...");
+        const port = await navigator.serial.requestPort();
+        if (port) {
+            updateFeedback('port opened');
+            // Redo the Init function to re-establish the serial connection
+            // init();
+            // Throw an event to let the app know that the serial connection is ready
+            const event = new CustomEvent('serialready', { detail: { port: port } });
+            document.dispatchEvent(event);
+
+            // Clean up the button
+            serialButton.remove();
+        }
+    }
+    serialButton.addEventListener('click', handleButtonClick);
 }
+
 function createSerialButton() {
-  console.log('---- Creating serial button ----');
-  if (document.getElementById('serialButton')) {
-      return;
-  }
-  const serialButton = document.createElement('button');
-  serialButton.textContent = 'Press to connect first available serial port';
-  serialButton.id = 'serialButton';
-  serialButton.style.display = 'inline-block';
-  serialButton.style.position = 'absolute';
-  serialButton.style.top = '50%';
-  serialButton.style.left = '50%';
-  serialButton.style.backgroundColor = '#FF0000';
-  serialButton.style.transform = 'translate(-50%, -50%)';
-  serialButton.style.fontSize = '1.15rem';
-  serialButton.style.padding = '1rem 1rem 2rem 1rem';
-  serialButton.style.cursor = 'pointer';
-  return document.body.appendChild(serialButton);
+    updateFeedback('Creating serial button...');
+    if (document.getElementById('serialButton')) {
+        return;
+    }
+    const serialButton = document.createElement('button');
+    serialButton.textContent = 'Press to connect first available serial port';
+    serialButton.id = 'serialButton';
+    serialButton.style.display = 'inline-block';
+    serialButton.style.position = 'absolute';
+    serialButton.style.top = '50%';
+    serialButton.style.left = '50%';
+    serialButton.style.backgroundColor = '#FF0000';
+    serialButton.style.transform = 'translate(-50%, -50%)';
+    serialButton.style.fontSize = '1.15rem';
+    serialButton.style.padding = '1rem 1rem 2rem 1rem';
+    serialButton.style.cursor = 'pointer';
+    return document.body.appendChild(serialButton);
 }
 
 /* ------------------------------------  
@@ -176,9 +199,8 @@ function createSerialButton() {
 * ------------------------------------ */
 
 function updateFeedback(message, ...args) {
-  console.log(`Serial Feedback: ${message}`, args);
+    console.log(`Feedback [Serial]: ${message}`, (args.length > 0) ? args : '');
 }
-
 
 export { connectSerial, serialSend };
 
